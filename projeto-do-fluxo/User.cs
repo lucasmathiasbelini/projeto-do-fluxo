9196fde917c22bbdd58b3bafaa27e94247e229bf
lucas <lucas.belini@escola.pr.gov.br>
2024-07-23 08:21:51 -0300

    joia

--- projeto-do-fluxo/User.cs
+++ projeto-do-fluxo/User.cs
@@ -10,17 +10,18 @@ public void Setemail(string email){
 public void Setid(){
 this.id = 1000;
 }
-public string Getemail(){
+public string Getemail(string email){
     return email;
 }
 
-public void Setemail(){
+public void Setemailstring(string email){
 if(this.email == null){
 
+ return" * acesso legado * ";
 
 }
 else{
-    return;
+ return" * acesso negado * ;
 }
 }
 }
